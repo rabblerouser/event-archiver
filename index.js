a34d1229d72773e5dad3d45c4187a4fc9673228d
Cam Jackson <camjackson89@gmail.com>
2017-03-02 09:49:34 +1100

    Parameterise the stream being watched during local development

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const handler = require('./src');
 
-const bucketName = process.env.ARCHIVE_BUCKET || 'rr-event-archive';
+const bucketName = process.env.ARCHIVE_BUCKET;
 const s3Endpoint = process.env.S3_ENDPOINT;
 const s3Config = s3Endpoint ?
   { endpoint: s3Endpoint, accessKeyId: 'FAKE', secretAccessKey: 'ALSO FAKE' } :
