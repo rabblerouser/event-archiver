2fe4266b62472a96eff990a96bfebf6ba5c73aee
phazel <cwebster@thoughtworks.com>
2017-07-03 14:15:33 +1000

    Pull when builing the docker image

--- bin/docker-build-and-deploy.sh
+++ bin/docker-build-and-deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 docker login -u "$DOCKER_USER" -p "$DOCKER_PASSWORD"
 
-docker build -t rabblerouser/event-archiver .
+docker build --pull -t rabblerouser/event-archiver .
 docker push rabblerouser/event-archiver
